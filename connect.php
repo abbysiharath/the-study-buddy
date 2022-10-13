ce03b50856a7ea3d5f1e09aeccb3a9f4a9fb0c98
DylanAdams2001 <dylana29@hotmail.com>
2022-10-13 13:36:15 +1100

    Update connect.php

--- connect.php
+++ connect.php
@@ -7,3 +7,16 @@
 <?
 
 //database connection using phpMyAdmin
+
+$conn = new mysqli('localhost','root','','test');
+if ($conn->connect_error){
+    die('Connection Failed  :  '.$conn->connect_error);
+}else{
+    $stmt = $conn->prepare("insert into registration(name, email, number) values(?, ?, ?, ?,");
+    $stmt->bind_param("sssi",$name, $email, $number);
+    $stmt->execute();
+    echo "registration Successful...";
+    $stmt->close();
+    $conn->close();
+
+}
